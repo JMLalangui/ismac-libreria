0760ee9c7c767298a87c16983e64ede6020e2597
JMLalangui <jlalangui@tecnologicoismac.edu.ec>
2024-08-19 12:57:14 -0500

    Update PrincipalAutor.java

--- Libreria/src/com/distribuida/principal/PrincipalAutor.java
+++ Libreria/src/com/distribuida/principal/PrincipalAutor.java
@@ -22,7 +22,7 @@ public class PrincipalAutor {
 				//autorDao.add(autorADD);
 				
 				//UP
-				Autor autorUP = new Autor(54,"nutella nutella","peña","ecuador","ascazubi","0987654321","nutella@gmail.com");
+				Autor autorUP = new Autor(54,"nutella nutella","peña","ecuar","ascazubi","0987654321","nutella@gmail.com");
 				autorDao.up(autorUP);
 				
 				//DELETE
